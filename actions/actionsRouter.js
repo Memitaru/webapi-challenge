622554eb5672bf2206677fa31f09d86008bc8b64
Memitaru <amandatwah@gmail.com>
2019-05-19 21:02:11 -0700

    Action post request now validates project id

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('../data/helpers/actionModel');
+const dbProjects = require('../data/helpers/projectModel')
 const router = express.Router();
 
 // Get all actions
@@ -53,19 +54,34 @@ router.put('/:id', validateID, async (req, res) => {
 // Add an action
 
 router.post('/', async (req, res) => {
-    try {
-        const newAction = db.insert(req.body)
-        if (!req.body.project_id) {
-            res.status(500).json({message: "You must include a project id number."})
-        } else if (req.body) {
-            res.json(newAction)
-        } else {
-            res.status(500).json({error: "Make sure you've included all necessary information."})
-        } 
-    } catch(err){
-        res.status(500).json(err.message)
+  try{
+    const validateProject = await dbProjects.get(req.body.project_id);
+
+    if(validateProject){
+        const newAction = await db.insert(req.body)
+        res.json(newAction)
+    } else {
+        res.status(404).json({message: "There is no project with that id"})
     }
+  } catch(err){
+      res.status(500).json(err.message)
+  }
 })
+
+// router.post('/', async (req, res) => {
+//     try {
+//         const newAction = db.insert(req.body)
+//         if (!req.body.project_id) {
+//             res.status(500).json({message: "You must include a project id number."})
+//         } else if (req.body) {
+//             res.json(newAction)
+//         } else {
+//             res.status(500).json({error: "Make sure you've included all necessary information."})
+//         } 
+//     } catch(err){
+//         res.status(500).json(err.message)
+//     }
+// })
 
 // Middleware
 
