7ba188f367f14dd8208a704af2d14cfbae02cd72
Memitaru <amandatwah@gmail.com>
2019-05-19 19:26:37 -0700

    Get request for specific project now works, created validate ID middleware

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -1,23 +1,42 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('../data/helpers/projectModel');
+const db = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
 // Get all projects
 
 router.get("/", async (req, res) => {
-    try {
-      const allProjects = await db.get(res.query);
-      if (allProjects) {
-        res.json(allProjects);
-      } else {
-          res.status(400).json({message: "No projects found."})
-      }
-    } catch (err) {
-      res.status(500).json({ message: "internal server errors" });
+  try {
+    const allProjects = await db.get(res.query);
+    if (allProjects) {
+      res.json(allProjects);
+    } else {
+      res.status(400).json({ message: "No projects found." });
     }
-  });
+  } catch (err) {
+    res.status(500).json({ message: "internal server errors" });
+  }
+});
 
+router.get('/:id', validateID, async (req, res) => {
+   const project = await db.get(req.params.id);
+   res.json(project)
+})
+
+// Middleware
+
+async function validateID(req, res, next) {
+    if (!req.params.id){
+        res.status(400).json({message: "Missing ID"})
+    } else {
+        const idValidated = await db.get(req.params.id);
+        if(!idValidated){
+            res.status(404).json({message: "ID is invalid"})
+            return; // Gets rid of unhandled promise rejection warning
+        }
+    }
+    next();
+}
 
 module.exports = router;
